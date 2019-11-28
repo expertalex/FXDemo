c4eab954fc5254dd1d2ddd0e59b6c494c2e675aa
@lex Bel <expertalex@users.noreply.github.com>
2019-11-28 09:53:08 +0100

    ReadMe

---
+++ FXDemo/Controllers/PlayersController.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FXDemo.Data;
+using FXDemo.Models;
+
+namespace FXDemo.Controllers
+{
+    public class PlayersController : Controller
+    {
+        private readonly FXDataContext _context;
+
+        public PlayersController(FXDataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Players
+        public async Task<IActionResult> Index()
+        {
+            var fXDataContext = _context.Player.Include(m => m.Team);
+            return View(await fXDataContext.ToListAsync());
+        }
+
+        // GET: Players/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var player = await _context.Player
+                .Include(m => m.Team)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            return View(player);
+        }
+
+        // GET: Players/Create
+        public IActionResult Create()
+        {
+            ViewData["TeamName"] = new SelectList(_context.Set<Team>(), "Id", "TeamName");
+            return View();
+        }
+
+        // POST: Players/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Number,YellowCards,RedCards,MinutesPlayed", "TeamName")] Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(player);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["TeamName"] = new SelectList(_context.Set<Team>(), "Id", "TeamName");
+            return View(player);
+        }
+
+        // GET: Players/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var player = await _context.Player.FindAsync(id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["TeamId"] = new SelectList(_context.Set<Team>(), "Id", "Name", "TeamName");
+            return View(player);
+        }
+
+        // POST: Players/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Number,YellowCards,RedCards,MinutesPlayed", "TeamId")] Player player)
+        {
+            if (id != player.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(player);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PlayerExists(player.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TeamId"] = new SelectList(_context.Set<Team>(), "Id", "Name", "TeamName");
+            return View(player);
+        }
+
+        // GET: Players/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var player = await _context.Player
+                .Include(m => m.Team)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            return View(player);
+        }
+
+        // POST: Players/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var player = await _context.Player.FindAsync(id);
+            _context.Player.Remove(player);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PlayerExists(int id)
+        {
+            return _context.Player.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,57 @@
+﻿# Interview Template (Football API)
+
+## Introduction
+
+This page was created to give general instructions to complete the Interview Test for FXStreet. 
+
+## Football API :soccer:
+
+The aim of this API is to store information about players, managers, referees, matches and league information. Each controller has 4 actions:
+
+* GET
+* POST
+* PUT
+* DELETE
+
+GOALS:
+
+1. Persist some examples of each resource. The technology chosen for persisting is up to you.
+2. You have to create a job that consume this endpoint (http://interview-api.azurewebsites.net/api/IncorrectAlignment) when.
+  - A match is about to start (5 minutes before).
+  - Send the List of Players or Managers that have 5 yellow cards or 1 red card.
+
+## Instructions
+
+
+1. Go to [this link](http://interview-api.azurewebsites.net/swagger/index.html) where we have placed the basic interface for the service.
+2. The aim of the test is to replicate the interface with a full functional Web API.
+3. The Web API must persist the data in whatever database you feel comfortable. (In case you want to use Azure contact developer@fxstreet.com to get the credentials).
+4. The service should be available via a public URL. (In case you want to use Azure contact developer@fxstreet.com to get the credentials).
+5. The code must be available in Github.
+6. Once you have finished the exercise send an Email (developer@fxstreet.com) following the Email Template described bellow.
+
+## Rules
+
+1. The code must be in .net Core.
+2. The code must be available in GitHub.
+3. The Web API must be public.
+4. The final result must be available in less than 5 days since the first interview.
+
+## Coding Tips
+
+1. The controller IncorrectAlignment is the endpoint that you need to consume for the GOAL 2 of the exercise.
+2. As department we strongly believe in Clean Code and SOLID Principles :heart_eyes:, show us that you too.
+3. We want to see your testing capabilities, despite is a CRUD web app, the second goal gives some room for it.
+4. If you feel comfortable you can use some DevOps tool (Azure Devops, jenkins, etc..) to deploy the Web API but is not compulsive.
+5. IMPORTANT :running: In case you didn't have the time to include certain capability/feature/technology comment them on the email and we will discuss it later on the interview.
+
+## Email Template
+
+Subject: FXStreet Interview - [Your full name]
+
+Body: 
+
+* Github Url
+* API Url 
+* Persistence Technology + Screenshot of the model
+* Comments
